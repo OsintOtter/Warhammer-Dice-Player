7ffa03084aacd7d89e22855c9054677025e9977c
OsintOtter <osintotter69@protonmail.com>
2023-02-22 00:37:04 -0500

    add initial files and framework

---
+++ code.js
@@ -0,0 +1,235 @@
+let x;
+let a;
+let b;
+let c;
+let d;
+let e;
+let f;
+let g; 
+let h;
+let i;
+let y = "x"; 
+
+document.getElementById("roll1").onclick = function(){
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+    document.getElementById("counter2").innerHTML = y;
+    document.getElementById("counter3").innerHTML = y;
+    document.getElementById("counter4").innerHTML = y;
+    document.getElementById("counter5").innerHTML = y;
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+   
+
+
+}
+
+document.getElementById("roll2").onclick = function(){
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+    document.getElementById("counter3").innerHTML = y;
+    document.getElementById("counter4").innerHTML = y;
+    document.getElementById("counter5").innerHTML = y;
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+   
+
+}
+
+document.getElementById("roll3").onclick = function(){
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter4").innerHTML = y;
+    document.getElementById("counter5").innerHTML = y;
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+
+
+}
+
+document.getElementById("roll4").onclick = function(){
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter5").innerHTML = y;
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("roll5").onclick = function(){
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("roll6").onclick = function(){
+    e = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter6").innerHTML = e;
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("roll7").onclick = function(){
+    f = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter7").innerHTML = f;
+    e = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter6").innerHTML = e;
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("roll8").onclick = function(){
+    g = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter8").innerHTML = g;
+    f = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter7").innerHTML = f;
+    e = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter6").innerHTML = e;
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("roll9").onclick = function(){
+    h = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter9").innerHTML = h;
+    g = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter8").innerHTML = g;
+    f = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter7").innerHTML = f;
+    e = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter6").innerHTML = e;
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+    document.getElementById("counter10").innerHTML = y;
+
+}
+
+document.getElementById("rollAll").onclick = function(){
+    i = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter10").innerHTML = i;
+    h = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter9").innerHTML = h;
+    g = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter8").innerHTML = g;
+    f = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter7").innerHTML = f;
+    e = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter6").innerHTML = e;
+    d = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter5").innerHTML = d;
+    c = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter4").innerHTML = c;
+    b = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter3").innerHTML = b;
+    a = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter2").innerHTML = a;
+    x = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById("counter1").innerHTML = x;
+
+}
+
+document.getElementById("clearBtn").onclick = function(){
+    document.getElementById("counter1").innerHTML = y;
+    document.getElementById("counter2").innerHTML = y;
+    document.getElementById("counter3").innerHTML = y;
+    document.getElementById("counter4").innerHTML = y;
+    document.getElementById("counter5").innerHTML = y;
+    document.getElementById("counter6").innerHTML = y;
+    document.getElementById("counter7").innerHTML = y;
+    document.getElementById("counter8").innerHTML = y;
+    document.getElementById("counter9").innerHTML = y;
+    document.getElementById("counter10").innerHTML = y;
+   
+    
+
+
+}
+
